8146be3ae1af426773d98133bd1d2699e71f859b
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2025-01-25 11:50:47 +0200

    added methods for user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepositoryService } from './repository/users-repository.service';
+import { Observable } from 'rxjs';
+import { User, UsersAPI } from './interface/users.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private usersRepositoryService: UsersRepositoryService) {}
+
+  getAll(
+    search?: string,
+    page?: string,
+    pageSize?: string,
+  ): Observable<UsersAPI> {
+    return this.usersRepositoryService.getAll(search, page, pageSize);
+  }
+
+  getOne(id: string): Observable<User> {
+    return this.usersRepositoryService.getOne(id);
+  }
+
+  getByUserName(username: string): Observable<User> {
+    return this.usersRepositoryService.getByUserName(username);
+  }
+}
